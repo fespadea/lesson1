d5209a82c2f42d7ac3632e9ba30c70678819eab3
Fernando  Spadea <fs279@pvhs-lab112-d10.pvsd.org>
2016-09-07 14:20:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("What is going on?");
+        System.out.print("Hello World");
+        //The above is the two ways we need to
+        // know how to output text
+        //Two types of comments
+        // 1. Line comment and that is done with a //
+        // 2. Block comment. that is done with a /*
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
